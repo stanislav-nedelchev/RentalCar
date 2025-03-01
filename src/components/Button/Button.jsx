2e5aeec9ea9ff9e217450337f9747b6b30df4184
stanislav_nedelchev <stasnedel@gmail.com>
2025-03-01 15:55:19 +0200

    fix button styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,60 +1,9 @@
-import css from "./Button.module.css";
+import css from './Button.module.css';
 
-const Button = ({ type, onClick, text, style }) => {
-  const buttonClass = `${css.button} ${css[style]}`;
-
-  return (
-    <button className={buttonClass} type={type} onClick={onClick}>
-      {text}
-    </button>
-  );
-};
+const Button = ({ type, onClick, text }) => (
+  <button className={css.button} type={type} onClick={onClick}>
+    {text}
+  </button>
+);
 
 export default Button;
-
-// const App = () => {
-//   const handleSave = () => {
-//     console.log("View Catalog");
-//   };
-
-//   const handleDelete = () => {
-//     console.log("Read more");
-//   };
-
-//   const handleEdit = () => {
-//     console.log("Search");
-//   };
-
-//   const handleCancel = () => {
-//     console.log("Send");
-//   };
-
-//   const handleLoad = () => {
-//     console.log("Load");
-//   };
-
-//   return (
-//     <div>
-//       <Button
-//         type="submit"
-//         style="long"
-//         text="View Catalog"
-//         onClick={handleSave}
-//       ></Button>
-//       <Button
-//         type="button"
-//         style="long"
-//         text="Read more"
-//         onClick={handleDelete}
-//       ></Button>
-//       <Button type="submit" text="Search" onClick={handleEdit}></Button>
-//       <Button type="submit" text="Send" onClick={handleCancel}></Button>
-//       <Button
-//         type="submit"
-//         style="load"
-//         text="Load more"
-//         onClick={handleLoad}
-//       ></Button>
-//     </div>
-//   );
-// };
